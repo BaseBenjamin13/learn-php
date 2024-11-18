4fcefb361779246ea3fa40997b3c36745e279872
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-11-17 23:00:18 -0500

    01

---
+++ 01_output.php
@@ -0,0 +1,21 @@
+<?php
+    // echo 123, ' hello ', 10.5;
+    // print 24;
+    // echo"<br>";
+    // echo"<br>";
+    // print_r([1,5,3,4]);
+    // var_dump('hello');
+    // var_dump(true);
+    // var_export([3,3,3]);
+?>
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   </head>
+   <body>
+    <h1><?= 'Post One';?></h1>
+   </body>
+   </html>
