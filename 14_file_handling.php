bb81917b7d0639ef93ce6436aac16b5b6f2e59cd
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-12-02 13:39:40 -0500

    add file upload

--- 14_file_handling.php
+++ 14_file_handling.php
@@ -4,13 +4,11 @@ $file = 'extras/users.txt';
 
 if(file_exists($file)) {
     // echo readfile($file);
-
     $handle = fopen($file, 'r');
     $contents = fread($handle, filesize($file));
     fclose($handle);
     echo $contents;
 } else {
-
     $handle = fopen($file, 'w');
     $contents = 'Brad' . PHP_EOL . 'Ben' . PHP_EOL . 'Jill' . PHP_EOL . 'Jack';
     fwrite($handle, $contents);
